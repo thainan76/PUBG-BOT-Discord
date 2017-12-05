86da4f44351a6329f9b50f011887ac3a49f9f16a
thainan76 <thainancpv76@hotmail.com>
2017-12-05 19:36:20 -0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -201,4 +201,4 @@ async def rank(ctx, username: str):
      text = str(t)
      await bot.say("Olá " + ctx.message.author.mention + " !" + text)
        
-bot.run('Mzg2MzkxNjUyMjAzMzY0MzUz.DQicGA.X5A-IxgOoZobP5d4gFuCZgSGp0U')
+bot.run('Mzg3NzA2MjQ5NTE4Nzc2MzIw.DQiixQ.3k7I3PvEi1h_xTRpexaYpwO_ZDs')
